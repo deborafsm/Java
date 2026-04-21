d278ac1c2541d503c98bc2e6efbef24578cbbc91
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2026-04-21 08:56:54 -0300

    add html

--- lendas-api/lendas-api/src/main/java/com/debora/lendas_api/LendaController.java
+++ lendas-api/lendas-api/src/main/java/com/debora/lendas_api/LendaController.java
@@ -2,6 +2,8 @@ package com.debora.lendas_api;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -9,26 +11,18 @@ import java.util.List;
 
 @RestController
 public class LendaController {
+    List<Lenda> lista = new ArrayList<>();
 
     @GetMapping("/lendas")
     public List<Lenda> listar(){
-
-        List<Lenda> list = new ArrayList<>();
-
-        Lenda l1 = new Lenda();
-        l1.nome = "Drizzt d'Urden";
-        l1.classe = "Ranger";
-        l1.raca = "Drow";
-
-        Lenda l2 = new Lenda();
-        l2.nome = "Zaknafein d'Urden";
-        l2.classe = "Espadachin";
-        l2.raca = "Drow";
-
-        list.add(l1);
-        list.add(l2);
-
-        return list;
+        return lista;
     }
+
+    @PostMapping("/lendas")
+    public Lenda criar(@RequestBody Lenda lenda){
+        lista.add(lenda);
+        return lenda;
+    }
+
 }
 
